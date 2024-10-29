3659e48dc03c2173a318b334134135b94a2fe6a8
niks <niks123@gmail.com>
2024-10-29 11:42:54 +0530

    deleting the files

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "backend_res"
-    storage_account_name = "tfstate_files"
-    container_name = "prod-tfstate"
-    key = "prod.terrform.tfstate"
-  }
-}
